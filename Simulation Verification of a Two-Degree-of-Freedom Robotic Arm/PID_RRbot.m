36e0463fbb17d634a51cee2ffd8a24f4588b9080
Li Guotai <212307020033@home.hpu.edu.cn>
2025-11-07 11:34:44 +0800

    Update PID_RRbot.m

--- Simulation Verification of a Two-Degree-of-Freedom Robotic Arm/PID_RRbot.m
+++ Simulation Verification of a Two-Degree-of-Freedom Robotic Arm/PID_RRbot.m
@@ -9,20 +9,20 @@ x0=[1,1,1,1,....
 options=odeset('MaxStep',0.01);
 [t,x]=ode45(@PID_RRbot_ode,[t0,tf],x0,options);
 
-A=[0,0,1,0;0,0,0,1;0,0,0,0;0,0,0,0];%RR-bot系统
-B=[0 0;0 0;1 0;0 1];%RR-bot系统
-Ar=[0,0,1,0;0,0,0,1;-1,0,-2,0;0,-1,0,-2];%参考系统
-Br=[0 0;0 0;1 0;0 1];%参考系统
+A=[0,0,1,0;0,0,0,1;0,0,0,0;0,0,0,0];
+B=[0 0;0 0;1 0;0 1];
+Ar=[0,0,1,0;0,0,0,1;-1,0,-2,0;0,-1,0,-2];
+Br=[0 0;0 0;1 0;0 1];
 
 %RR-+-
 
 % -+++++++++++++++++++++++++++++++++++
                                                                                                                              
-a0=1;%连杆1长度为1
-a1=1;%连杆2长度为1
-m1=0.1;%连杆1的质量为0.1kg
-m2=0.1;%连杆2的质量为0.2kg
-gs=9.8;%重力加速度
+a0=1;
+a1=1;
+m1=0.1;
+m2=0.1;
+gs=9.8;
 
 Wd=[2,0,1,0;...
     0,3,0,1]';
@@ -45,7 +45,7 @@ for i=1:length(t)
 end
 
 r1=sin(t);r2=cos(t);
-% r1=zeros(size(t));%列向量
+% r1=zeros(size(t));
 % r2=zeros(size(t));
 % for i=1:length(t)
 %     if t(i)>=20 && t(i)<50
@@ -73,44 +73,40 @@ for i=1:size(t,1)
     U(2,i)=Kp(2)*e21(i)'+Kd(2)*e22(i)+Ki(2)*x(i,10);
 end
 
-%位置跟踪
 Xref1_11=x(:,5);
 Xref1_12=x(:,7);
 X1_1=x(:,1);
 X1_2=x(:,3);
-%速度跟踪
+
 Xref1_21=x(:,6);
 Xref1_22=x(:,8);
 X1_3=x(:,2);
 X1_4=x(:,4);
-% 跟踪误差
+
 E1_11=e11;
 E1_12=e12;
 E1_21=e21;
 E1_22=e22;
-%输入力矩
 U1=U;
 
-% %非线性部分
+
 % yref2_1=y_true(1,:)';
 % yref2_2=y_true(2,:)';
-% %逼近误差
 % EE2_1=y_ref1-y_hat1;
 % EE2_2=y_ref2-y_hat2;
 
 % 
-% disp(['状态1:',num2str(sum(abs(e(:,1)))),';'])
-% disp(['状态2:',num2str(sum(abs(e(:,2)))),';'])
-% disp(['状态3:',num2str(sum(abs(e(:,3)))),';'])
-% disp(['状态4:',num2str(sum(abs(e(:,4)))),'.'])
-% disp(['状态1:',num2str(sum(abs(e(:,1)))/length(t))])
-% disp(['状态2:',num2str(sum(abs(e(:,2)))/length(t))])
-% disp(['状态3:',num2str(sum(abs(e(:,3)))/length(t))])
-% disp(['状态4:',num2str(sum(abs(e(:,4)))/length(t))])
+% disp(['state 1:',num2str(sum(abs(e(:,1)))),';'])
+% disp(['state 2:',num2str(sum(abs(e(:,2)))),';'])
+% disp(['state 3:',num2str(sum(abs(e(:,3)))),';'])
+% disp(['state 4:',num2str(sum(abs(e(:,4)))),'.'])
+% disp(['state 1:',num2str(sum(abs(e(:,1)))/length(t))])
+% disp(['state 2:',num2str(sum(abs(e(:,2)))/length(t))])
+% disp(['state 3:',num2str(sum(abs(e(:,3)))/length(t))])
+% disp(['state 4:',num2str(sum(abs(e(:,4)))/length(t))])
 % 
 % 
 % 
-% %状态
 % figure()
 % subplot(221)
 % plot(t,x(:,1),'-r')
@@ -140,15 +136,16 @@ U1=U;
 % legend('x4-dyn','x4-ref')
 % xlabel('time(t/s)')
 % ylabel('state4 value')
-% %误差
+
 % figure()
 % subplot(221);plot(t,e11);grid on;legend('e11');
 % subplot(222);plot(t,e21);grid on;legend('e21');
 % subplot(223);plot(t,e12);grid on;legend('e12');
 % subplot(224);plot(t,e22);grid on;legend('e22');
-% %控制
+
 % figure()
 % plot(t,U');grid on
 % legend('U1','U2')
 % xlabel('time(t/s)')
+
 % ylabel('control signal')
